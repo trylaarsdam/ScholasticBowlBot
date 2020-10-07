aaa31387b1f667275f216948350ca9538dd0ebce
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-10-07 11:12:02 -0500

    fixed roles for v12

--- index.js
+++ index.js
@@ -182,8 +182,8 @@ client.on('message', msg => {
     }
     else if (msg.content.startsWith("!team")){
         var content = [];
-        let team1 = msg.guild.roles.find(r => r.name === "Team 1");
-        let team2 = msg.guild.roles.find(r => r.name === "Team 2");
+        let team1 = msg.guild.roles.get("Team 1");
+        let team2 = msg.guild.roles.get("Team 2");
         content = msg.content.split(" ");
         if(content[1] == '1'){
             msg.member.roles.add(team1);
