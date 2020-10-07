653b7789072c1b923c07b01cc56e78ac747f0c70
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-10-06 22:45:26 -0500

    fix

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ client.on('message', msg => {
                 })
                 inc = inc + 1;
             })
+            msg.channel.send({buzzList})
         }
     }
     else if (msg.content === '!reset'){
