00c4d04e9f3e348055259225d2b75ac42f79baa1
Todd Rylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-10-06 21:08:20 -0500

    role search

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ client.on('message', msg => {
             msg.reply("Muted everyone in channel");
             let channel = msg.member.voice.channel;
             channel.members.forEach((member) => {
-                if(!member.roles.find(r => r.name === "Mute Exempt")){
+                if(!member.roles.some(role => role.name === 'Mute Exempt')){
                     member.setMute(true);
                 }
             })
